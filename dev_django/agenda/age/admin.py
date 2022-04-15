5738f363baed06831bb2748d1997277c59d17d32
Flavio <82716912+flavio641@users.noreply.github.com>
2022-04-15 19:57:55 -0300

    CriaçãoTabelas

--- dev_django/agenda/age/admin.py
+++ dev_django/agenda/age/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from age.models import Evento
 
 # Register your models here.
+
+class Eventoadmin(admin.ModelAdmin):
+    list_display= ('titulo','data_evento','data_criacao')
+
+
+admin.site.register(Evento, Eventoadmin)
--- dev_django/agenda/age/models.py
+++ dev_django/agenda/age/models.py
@@ -1,3 +1,14 @@
+from pickle import TRUE
 from django.db import models
 
 # Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True,null=TRUE)
+    data_evento = models.DateTimeField()
+    data_criacao = models.DateTimeField(auto_now=True)
+
+
+def __str__(self):
+    return self.titulo

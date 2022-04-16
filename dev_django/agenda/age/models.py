0dc01d1f12c6253b0b227aa3d0d164a93f88a860
Flavio <82716912+flavio641@users.noreply.github.com>
2022-04-16 12:27:07 -0300

    mais um passo no codigo

--- dev_django/agenda/age/models.py
+++ dev_django/agenda/age/models.py
@@ -1,13 +1,16 @@
 from pickle import TRUE
+from tabnanny import verbose
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True,null=TRUE)
-    data_evento = models.DateTimeField()
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
     data_criacao = models.DateTimeField(auto_now=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 def __str__(self):
